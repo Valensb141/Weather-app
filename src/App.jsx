6d739f4a1cc08b2cdcdfe7d42155a9b522f4929c
Valensb141 <116755122+Valensb141@users.noreply.github.com>
2023-06-10 18:25:34 +0200

    styles added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,40 @@
 import axios from 'axios'
-import { useState } from 'react'
+import { useEffect } from 'react'
 
 function App() {
-  
+
+  //const url =`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/London%2CUK?unitGroup=us&key=YG62GNRYM6CC9HTBJWWH3NW9G`
+
 
   return (
     <>
-     <h1>Hello</h1>
+     <div className="app">
+
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <p>Dallas</p>
+          </div>
+          <div className="temp">
+            <h1>60ºF</h1>
+          </div>
+          <div className="description">
+            <p>Clouds</p>
+          </div>
+        </div>
+        <div className="bottom">
+          <div className="feels">
+            <p>60ºF</p>
+          </div>
+          <div className="humidity">
+            <p>20%</p>
+          </div>
+          <div className="wind">
+            12MPH
+          </div>
+        </div>
+      </div>
+    </div>
     </>
   )
 }
